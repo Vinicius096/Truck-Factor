3808e2cef4d9685989640789bc37a82167dd9754
Guilherme Avelino <gavelino@gmail.com>
2017-07-27 16:08:41 +0200

    minor

--- gittruckfactor/get_first_files_commits.sh
+++ gittruckfactor/get_first_files_commits.sh
@@ -26,6 +26,6 @@ else
   while IFS=";" read file
   	do
         sha=$(git log --follow --pretty=format:"%H" "$file"  | tail -1)
-    echo "$file;$sha"   	
-  done < filelist.log >> files_comit_sha.log
+    echo "$file;$sha" >> files_comit_sha.log  	
+  done < filelist.log 
 fi
