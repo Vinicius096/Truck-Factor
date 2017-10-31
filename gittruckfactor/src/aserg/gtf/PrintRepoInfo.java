63394b22f8e4fe4de2a70295aede2a4c0e972cba
Guilherme Avelino <gavelino@gmail.com>
2017-10-31 17:11:38 -0300

    Collect leavers and repositories commits number

---
+++ gittruckfactor/src/aserg/gtf/PrintRepoInfo.java
@@ -0,0 +1,29 @@
+package aserg.gtf;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import aserg.gtf.dao.newstudy.MeasureDAO;
+
+public class PrintRepoInfo {
+	static final String INVESTIGATION_DATE = "2017-07-18 06:52:59.771";
+	public static void main(String[] args) throws IOException {
+		String result;
+//		result = new MeasureDAO().getNumberOfCommitsDataAll("month", null);
+//		result = new MeasureDAO().getNumberOfCommitsDataAll("month", INVESTIGATION_DATE);
+//		result = new MeasureDAO().getNumberOfCommitsDataAllLeavers("month", null);
+//		System.out.println(result);
+		Files.write(Paths.get("./leaverscommits_weekly.csv"), new MeasureDAO().getNumberOfCommitsDataAllLeavers("week", INVESTIGATION_DATE).getBytes());
+		System.out.println("SAVE leaverscommits_weekly,csv file");
+		
+		Files.write(Paths.get("./leaverscommits_daily.csv"), new MeasureDAO().getNumberOfCommitsDataAllLeavers("day", INVESTIGATION_DATE).getBytes());
+		System.out.println("SAVE leaverscommits_daily,csv file");
+		
+		Files.write(Paths.get("./commits_daily.csv"), new MeasureDAO().getNumberOfCommitsDataAll("day", INVESTIGATION_DATE).getBytes());
+		System.out.println("SAVE commits_daily,csv file");
+				
+//		Files.write(Paths.get("./commits_weekly.csv"), new MeasureDAO().getNumberOfCommitsDataAll("week", INVESTIGATION_DATE).getBytes());
+	}
+
+}
--- gittruckfactor/src/aserg/gtf/dao/newstudy/MeasureDAO.java
+++ gittruckfactor/src/aserg/gtf/dao/newstudy/MeasureDAO.java
@@ -1,6 +1,12 @@
 package aserg.gtf.dao.newstudy;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+
 import aserg.gtf.dao.GenericDAO;
+import aserg.gtf.model.newstudy.Leaver;
 import aserg.gtf.model.newstudy.Measure;
 
 public class MeasureDAO extends GenericDAO<Measure>{
@@ -14,7 +20,147 @@ public class MeasureDAO extends GenericDAO<Measure>{
 	public boolean exist(Measure entity) {
 		return this.find(entity.getId())!=null;
 	}
-	
-	
 
+	public String getNumberOfCommitsData(String repositoryName, String timeframe)
+	{
+		String strRes="";
+		String hql = " WITH "
+				+ "repocommits AS (SELECT * FROM logcommitinfo WHERE  repositoryname = \'"+ repositoryName + "\'), "
+				+ "maxDate AS (SELECT max(committerdate) FROM repocommits), "
+				+ "grid AS ( SELECT start_time, lead(start_time) OVER (ORDER BY start_time) AS end_time FROM  "
+				+ "(SELECT generate_series(min(committerdate), max(committerdate) + interval \'1 " + timeframe +"\', interval \'1 " + timeframe +"\') AS start_time "
+						+ "FROM   repocommits  ) x ) "
+				+ "SELECT TO_CHAR(start_time, 'DD-MM-YYYY'), count(DISTINCT e.sha) AS events FROM grid g "
+					+ "LEFT JOIN repocommits e ON e.committerdate >= g.start_time "
+					+ "AND e.committerdate <  g.end_time "
+					+ "WHERE g.start_time <= (SELECT max FROM maxDate) "
+					+ "GROUP  BY 1 ORDER  BY 1;";
+		Query q = this.em.createNativeQuery(hql);
+		List resultList = q.getResultList();
+		for (Object object : resultList) {
+			Object o[] = (Object[]) object;
+			strRes+=repositoryName+";"+o[0]+";"+o[1]+"\n";
+		}
+		return strRes;
+	}
+	
+	public String getNumberOfCommitsDataAll(String timeframe, String computedDate)
+	{
+		String strRes="repository;date;ncommits\n";
+		String hql;
+		if (computedDate == null)
+			hql = "SELECT repositoryname FROM measure m "
+				+ "WHERE istfevent "
+				+ "GROUP BY repositoryname;";
+		else
+			hql = "SELECT repositoryname FROM measure m "
+				+ "WHERE istfevent and computeddate = \'" + computedDate + "\' "
+				+ "GROUP BY repositoryname;";
+		Query q = this.em.createNativeQuery(hql);
+		List resultList = q.getResultList();
+		for (Object object : resultList) {
+			strRes+=getNumberOfCommitsData((String)object, timeframe);
+		}
+		return strRes;
+	}
+	
+	public String getNumberOfLeaversCommits(String repositoryName, String leaverName, String timeframe)
+	{
+		String strRes="";
+		String hql = " WITH "
+				+ "leavercommits AS (SELECT lci.* FROM measure m "
+					+ "JOIN measure_leaver ml ON ml.measure_id = m.id "
+					+ "JOIN leaver l ON ml.leavers_id = l.id "
+					+ "JOIN leaver_logcommitinfo ll ON ll.leaver_id = l.id "
+					+ "JOIN logcommitinfo lci ON ll.commits_id = lci.id "
+					+ "WHERE istfevent AND m.repositoryname = \'"+ repositoryName + "\' "
+							+ "AND l.username = \'"+ leaverName + "\'), "
+				+ "maxDate AS (SELECT max(committerdate) FROM leavercommits), "
+				+ "grid AS ( SELECT start_time, lead(start_time) OVER (ORDER BY start_time) AS end_time FROM  "
+				+ "(SELECT generate_series(min(committerdate), max(committerdate) + interval \'1 " + timeframe +"\', interval \'1 " + timeframe +"\') AS start_time "
+						+ "FROM   leavercommits  ) x ) "
+				+ "SELECT TO_CHAR(start_time, 'DD-MM-YYYY'), count(DISTINCT e.sha) AS events FROM grid g "
+					+ "LEFT JOIN leavercommits e ON e.committerdate >= g.start_time "
+					+ "AND e.committerdate <  g.end_time "
+					+ "WHERE g.start_time <= (SELECT max FROM maxDate) "
+					+ "GROUP  BY 1 ORDER  BY 1;";
+		Query q = this.em.createNativeQuery(hql);
+		List resultList = q.getResultList();
+		for (Object object : resultList) {
+			Object o[] = (Object[]) object;
+			strRes+=repositoryName+";"+leaverName+";"+o[0]+";"+o[1]+"\n";
+		}
+		return strRes;
+	}
+	
+	public String getNumberOfCommitsDataAllLeavers(String timeframe, String computedDate)
+	{
+		String strRes="repository;leaver;date;ncommits\n";
+		String hql;
+		if (computedDate == null)
+			hql = "SELECT DISTINCT m.repositoryname, l.username FROM measure m "
+					+ "JOIN measure_leaver ml ON ml.measure_id = m.id "
+					+ "JOIN leaver l ON ml.leavers_id = l.id "
+					+ "JOIN leaver_logcommitinfo ll ON ll.leaver_id = l.id "
+					+ "JOIN logcommitinfo lci ON ll.commits_id = lci.id "
+						+ "WHERE istfevent  ORDER BY m.repositoryname, l.username;";
+		else
+			hql = "SELECT DISTINCT m.repositoryname, l.username FROM measure m "
+					+ "JOIN measure_leaver ml ON ml.measure_id = m.id "
+					+ "JOIN leaver l ON ml.leavers_id = l.id "
+					+ "JOIN leaver_logcommitinfo ll ON ll.leaver_id = l.id "
+					+ "JOIN logcommitinfo lci ON ll.commits_id = lci.id "
+						+ "WHERE istfevent and computeddate = \'" + computedDate + "\' ORDER BY m.repositoryname, l.username;";
+		Query q = this.em.createNativeQuery(hql);
+		List resultList = q.getResultList();
+		for (Object object : resultList) {
+			Object o[] = (Object[]) object;
+			strRes+=getNumberOfLeaversCommits((String)o[0], (String)o[1], timeframe);
+		}
+		return strRes;
+	}
+	
+	
+	
+//	public String getNumberOfCommitsData(String repositoryName, String timeframe, String tfEventDate)
+//	{
+//		String strRes="";
+//		String hql = " WITH grid AS ( SELECT start_time, lead(start_time) OVER (ORDER BY start_time) AS end_time FROM  "
+//				+ "(SELECT generate_series(min(committerdate), max(committerdate), interval \'1 " + timeframe +"\') AS start_time "
+//						+ "FROM   logcommitinfo WHERE  repositoryname = \'"+ repositoryName + "\' ) x ) "
+//				+ "SELECT TO_CHAR(start_time, 'DD-MM-YYYY'), count(DISTINCT e.sha) AS events FROM grid g "
+//					+ "LEFT JOIN logcommitinfo e ON e.committerdate >= g.start_time "
+//					+ "AND e.committerdate <  g.end_time "
+//					+ "AND repositoryname = \'"+ repositoryName + "\' "
+//					+ "GROUP  BY 1 ORDER  BY 1;";
+//		Query q = this.em.createNativeQuery(hql);
+//		List resultList = q.getResultList();
+//		for (Object object : resultList) {
+//			Object o[] = (Object[]) object;
+//			strRes+=repositoryName+";"+o[0]+";"+o[1]+"\n";
+//		}
+//		return strRes;
+//	}
+//	public String getNumberOfCommitsDataAllTFEvent(String timeframe, String computedDate)
+//	{
+//		String strRes="repository;date;ncommits\n";
+//		String hql;
+//		if (computedDate == null)
+//			hql = "SELECT repositoryname, lasttfleaverdate FROM measure m "
+//				+ "WHERE istfevent "
+//				+ "GROUP BY repositoryname, lasttfleaverdate; "
+//				+ "ORDER BY repositoryname, lasttfleaverdate;";
+//		else
+//			hql = "SELECT repositoryname, lasttfleaverdate FROM measure m "
+//				+ "WHERE istfevent and computeddate = \'" + computedDate + "\' "
+//				+ "GROUP BY repositoryname, lasttfleaverdate; "
+//				+ "ORDER BY repositoryname, lasttfleaverdate;";
+//		Query q = this.em.createNativeQuery(hql);
+//		List resultList = q.getResultList();
+//		for (Object object : resultList) {
+//			Object[] data = (Object[])object;
+//			strRes+=getNumberOfCommitsData((String)data[0], timeframe, (String)data[1]);
+//		}
+//		return strRes;
+//	}
 }
